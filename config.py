2e9b539dc1b8ab3972a87f5f208b69eba52a69ac
xtrahot1 <galahan1987@gmail.com>
2025-08-14 17:39:53 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+import os
+
+
 class Config:
     SECRET_KEY = 'your-secret-key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///rice_disease.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'app/static/uploads'
+    UPLOAD_FOLDER3 = 'app/static/uploads/train'
+    
+    
--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='192.168.1.9', port=5000)
+    app.run(debug=True)
+    # app.run(host='192.168.1.9', port=5000)
